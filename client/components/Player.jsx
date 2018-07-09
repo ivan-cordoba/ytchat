6588af6d79d97962858ee01e090f329137dfb319
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-07-08 17:06:34 -0700

    style change

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -3,7 +3,7 @@ import YouTube from 'react-youtube';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 50vw;
+  width: 40vw;
 `;
 
 export default class Player extends React.Component {
